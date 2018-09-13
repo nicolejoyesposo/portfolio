30d2a5c52567ca386a1700745a04f6438351e218
Nicole Esposo <nicolejoyesposo.t@gmail.com>
2018-09-14 02:13:21 +0800

    portfolio original v.1

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nicole Esposo</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- font awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!-- jquery -->
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
+	</script>
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  	<!-- css file -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<!-- js file -->
+	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src="assets/js/circle-progress.js"></script>
+
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Noto+Serif+JP|Montserrat|Syncopate" rel="stylesheet">
+
+	<!-- aos -->
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+</head>
+<body>
+
+	<nav>
+		<div id="social-icons">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-gitlab"></i></a>
+			<a href="#"><i class="fab fa-github"></i></a>
+		</div>
+		<div id="logo">
+			<img src="assets/images/logo4.png">
+		</div>	
+		<div id="burger">
+			<i class="fas fa-bars"></i>
+		</div>	
+		<div id="menu">
+			<a href="#bg">Home</a>
+			<a href="#about">About</a>
+			<a href="#skills">Skills</a>
+			<a href="#bg3">Portfolio</a>
+			<a href="#">Contact</a>
+			<div id="social-icons1">
+				<a href="#"><i class="fab fa-facebook-f"></i></a>
+				<a href="#"><i class="fab fa-instagram"></i></a>
+				<a href="#"><i class="fab fa-gitlab"></i></a>
+				<a href="#"><i class="fab fa-github"></i></a>
+			</div>
+		</div>
+	</nav>
+
+	<!-- header -->
+	<div class="container-fluid" id="bg">
+		<div class="row">
+			<div class="col-md-4 offset-md-2">
+				<div class="headline">
+					<p id="hello">HELLO! I'M NICOLE.</p>
+					<p id="webdev">WEB DEVELOPER</p>
+					<hr>
+					<p id="webdev-txt">I am a fresh graduate who loves to develop and design websites.</p>
+					<a href="assets/resume-pdf.pdf" class="btn btn-danger btn-lg">HIRE ME</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!-- about -->
+<div id="about">
+	<div class="container">
+		<div class="row"> <!-- start of row -->
+			<div class="col-md-12 section-title">
+				<p>S O M E T H I N G</p>
+				<p class="section-main-title">ABOUT ME</p>	
+			</div>
+
+			<div class="col-md-4" id="left-about">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="single-description-left" data-aos="fade-right">
+							<p class="img-description-title">FULL NAME</p>
+							<p>Nicole Joy Estos-Esposo</p>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="single-description-left" data-aos="fade-right">
+							<p class="img-description-title">COURSE</p>
+							<p>BS in Aviation Information Technology</p>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="single-description-left" data-aos="fade-right">
+							<p class="img-description-title">COLLEGE</p>
+							<p>Philippine State College of Aeronautics</p>
+						</div>	
+					</div>
+				</div>	
+			</div>
+
+			<!-- image -->
+			<div class="col-md-4 my-image" data-aos="fade-down">
+				<img src="assets/images/me1.jpg">
+			</div>
+			<!-- image -->
+
+			<div class="col-md-4" id="right-about">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="single-description-left" data-aos="fade-left">
+							<p class="img-description-title">ADDRESS</p>
+							<p>Central Signal Village, Taguig City</p>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="single-description-left" data-aos="fade-left">
+							<p class="img-description-title">BIRTHDAY</p>
+							<p>May 1, 1998</p>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="single-description-left" data-aos="fade-left">
+							<p class="img-description-title">EMAIL</p>
+							<p>nicolejoyesposo.t@gmail.com</p>
+						</div>	
+					</div>
+				</div>	
+			</div>
+		</div> <!-- end of row -->
+	</div>
+</div>
+
+<!-- skills -->
+<div class="skills" id="skills">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 section-title2">
+				<p><span class="change-color">My</span> Skills</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3 col-sm-3" data-aos="fade-up">
+				<i class="fas fa-pencil-ruler fa-3x"></i>
+				<p>WEB DESIGN & UI</p>
+			</div>
+			<div class="col-md-3 col-sm-3" data-aos="fade-up">
+				<i class="fas fa-laptop-code fa-3x"></i>
+				<p>DEVELOPMENT</p>
+			</div>
+			<div class="col-md-3 col-sm-3" data-aos="fade-up">
+				<i class="fas fa-mobile-alt fa-3x"></i>
+				<p>RESPONSIVE</p>
+			</div>
+			<div class="col-md-3 col-sm-3" data-aos="fade-up">
+				<i class="fas fa-align-right fa-3x"></i>
+				<p>ORGANIZED CODES</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- portfolio -->
+<div class="container-fluid" id="bg3">
+  <div class="row">
+	<div class="col-md-12 section-title1">
+		<p>E X P L O R E</p>
+		<p class="section-main-title">MY WORKS</p>	
+	</div>
+    <div class="col-md-4">
+      <div class="polaroid" data-aos="fade-down" data-aos-easing="linear"
+     data-aos-duration="500">
+        <figure>
+          <img src="assets/images/site1.png">
+        </figure>
+        <p><a href="https://nicolejoyesposo.github.io/kalon-flair/">Kalon-Flair</a></p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="polaroid" data-aos="fade-down" data-aos-easing="linear"
+     data-aos-duration="500">
+        <figure>
+          <img src="assets/images/site2.png">
+        </figure>
+        <p><a href="https://i-do-knot.000webhostapp.com/">IDoKnot</a></p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="polaroid last" data-aos="fade-down" data-aos-easing="linear"
+     data-aos-duration="500">
+        <figure>
+          <img src="assets/images/site3.png">
+        </figure>
+        <p><a href="https://unspokenwords.000webhostapp.com/">Unspoken Words</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- contact -->
+<div class="container-container" id="contact">
+	<div class="row">
+		<div class="col-md-4 offset-md-3">
+			<div class="contact-details">
+				<p class="contact-title">Contact Me</p>
+				<div class="row">
+					<div class="col-md-2">
+						<i class="fab fa-facebook fa-2x"></i>
+					</div>
+					<div class="col-md-10">
+						<p><a href="https://www.facebook.com/lalalafever">https://www.facebook.com/lalalafever</a></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2">
+						<i class="fab fa-gitlab fa-2x"></i>
+					</div>
+					<div class="col-md-10">
+						<p><a href="https://gitlab.com/nicolejoyesposo">https://gitlab.com/nicolejoyesposo</a></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2">
+						<i class="far fa-envelope fa-2x"></i>
+					</div>
+					<div class="col-md-10">
+						<p><a href="mailto:nicolejoyesposo.t@gmail.com" target="_top">nicolejoyesposo.t@gmail.com</a></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2">
+						<i class="fas fa-phone fa-2x"></i>
+					</div>
+					<div class="col-md-10">
+						<p>+639955567558</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 img-contact">
+			<figure>
+				<img src="assets/images/i.jpg">
+			</figure>
+		</div>
+	</div>
+</div>
+
+
+<footer>
+	<p>/* Made With Love */</p>
+	<span>&copy Nicole Joy Esposo - 2018</span>
+</footer>
+
+<script type="text/javascript">
+
+	$(document).ready(function() {
+			$('#burger i').click(function() {
+				$('#menu').toggle();
+			});
+		});
+
+  AOS.init();
+
+</script>
+
+
+</body>
+</html>
